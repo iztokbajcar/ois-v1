65debad1851967b7e8c6710ca8a8bc81377660a0
Flowizard2 <tl9942@student.uni-lj.si>
2021-02-27 17:29:45 +0100

    #3 Dodajanje izracuna pospeska

--- Gravitacija.java
+++ Gravitacija.java
@@ -3,5 +3,10 @@ public class Gravitacija {
 	public static void main(String[] args) {
 		System.out.println("OIS je zakon!");
 	}
-
+	public static double izracunGravitacije(double v){
+		double C = 6.674e-11;
+		double M = 5.972e24;
+		double r = 6.371e6;
+		return (C * M) / (Math.pow(r + v, 2.0));
+	}
 }
